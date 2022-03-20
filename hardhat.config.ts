56a367111bef71a29b5f0dfa5a1a5e74f7489d61
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-03-20 15:23:07 +0900

    chore: Added Hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,59 @@
+import type { HardhatUserConfig } from "hardhat/types";
+import { task } from "hardhat/config";
+
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-abi-exporter";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "dotenv/config";
+
+task("accounts", "Prints the list of accounts", async (_args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  accounts.forEach(async (account) => console.info(account.address));
+});
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+      mining: {
+        auto: true,
+        interval: 10000,
+      },
+      gasPrice: "auto",
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: { optimizer: { enabled: true, runs: 888888 } },
+      },
+      {
+        version: "0.4.18",
+        settings: { optimizer: { enabled: true, runs: 999 } },
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts/",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  abiExporter: {
+    path: "./abis",
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    except: ["AggregatorTest.sol", "ICheatCodes.sol", "Mock*"],
+  },
+};
+
+export default config;
