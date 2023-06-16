10a91633b34424551d69c115b9399fbbb49011cb
hiroshi <hiroshi@looksrare.org>
2023-06-16 16:56:15 +0100

    build: Hardhat only works with 0.8.19

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,7 +33,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.20",
+        version: "0.8.19",
         settings: { optimizer: { enabled: true, runs: 888888 } },
       },
       {
