82514f5945173b086f535c1e7cdf8bcc99e8c417
hiroshi <hiroshi@looksrare.org>
2023-06-16 15:17:40 +0100

    feat: SeasonRewardsDistributor

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,6 +33,10 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
+        version: "0.8.20",
+        settings: { optimizer: { enabled: true, runs: 888888 } },
+      },
+      {
         version: "0.8.7",
         settings: { optimizer: { enabled: true, runs: 888888 } },
       },
