a8653e8eb4ab55f37f4c0927f2b7fa1751b8ba29
hiroshi <hiroshi@looksrare.org>
2023-06-16 18:08:59 +0100

    fix: Address should be checksummed

--- scripts/deployment/SeasonRewardsDistributorDeployment.s.sol
+++ scripts/deployment/SeasonRewardsDistributorDeployment.s.sol
@@ -18,7 +18,7 @@ contract SeasonRewardsDistributorDeployment is Script {
         uint256 deployerPrivateKey;
 
         if (chainId == 1) {
-            looksRareToken = 0xf4d2888d29d722226fafa5d9b24f9164c092421e;
+            looksRareToken = 0xf4d2888d29D722226FafA5d9B24F9164c092421E;
             owner = 0xBfb6669Ef4C4c71ae6E722526B1B8d7d9ff9a019;
             deployerPrivateKey = vm.envUint("MAINNET_KEY");
         } else if (chainId == 5) {
